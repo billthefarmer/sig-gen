d50074e0be33267b9f04c69688ece737dd3551fe
Bill Farmer <williamjfarmer@yahoo.co.uk>
2013-06-02 10:52:06 +0100

    Add animation for knob

--- src/org/billthefarmer/siggen/Knob.java
+++ src/org/billthefarmer/siggen/Knob.java
@@ -23,6 +23,7 @@
 
 package org.billthefarmer.siggen;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -35,10 +36,13 @@ import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Shader.TileMode;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 
 public class Knob extends View
+    implements GestureDetector.OnGestureListener, ValueAnimator.AnimatorUpdateListener
 {
     protected static final int MARGIN = 8;
 
@@ -59,6 +63,8 @@ public class Knob extends View
     private Paint paint;
     private LinearGradient gradient;
     private LinearGradient dimple;
+    private GestureDetector detector;
+    private ValueAnimator animator;
 
     private Bitmap next;
     private Bitmap previous;
@@ -72,12 +78,14 @@ public class Knob extends View
 	Resources resources = getResources();
 
 	next = BitmapFactory.decodeResource(resources,
-		      R.drawable.ic_navigation_next_item);
+					    R.drawable.ic_navigation_next_item);
 
 	previous = BitmapFactory.decodeResource(resources,
-		      R.drawable.ic_navigation_previous_item);
+						R.drawable.ic_navigation_previous_item);
 
 	matrix = new Matrix();
+
+	detector = new GestureDetector(context, this);
     }
 
     @Override
@@ -110,18 +118,18 @@ public class Knob extends View
 	height = h;
 		
 	paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-	gradient = new LinearGradient(0, -h / 2, 0, h / 2,
-			Color.WHITE, Color.GRAY, TileMode.CLAMP);
-	dimple = new LinearGradient(MARGIN / 2, 0, MARGIN / 2, MARGIN,
-			Color.GRAY, Color.WHITE, TileMode.CLAMP);
+	gradient = new LinearGradient(0, -h * 2 / 3, 0, h * 2 / 3,
+				      Color.WHITE, Color.GRAY, TileMode.CLAMP);
+	dimple = new LinearGradient(MARGIN / 2, -MARGIN / 2, MARGIN / 2, MARGIN / 2,
+				    Color.GRAY, Color.WHITE, TileMode.CLAMP);
     }
 
     @Override
     protected void onDraw(Canvas canvas)
     {
-    canvas.translate(width / 2, height / 2);
+	canvas.translate(width / 2, height / 2);
 
-    paint.setShader(gradient);
+	paint.setShader(gradient);
 	paint.setStyle(Style.FILL);
 
 	int radius = Math.min(width, height) / 2;
@@ -135,14 +143,14 @@ public class Knob extends View
 	float y = (float) (-Math.cos(value * Math.PI / 100) * radius * 0.8);
 
 	paint.setShader(dimple);
-	matrix.setTranslate(x - MARGIN / 2, y - MARGIN /2);
+	matrix.setTranslate(x, y);
 	dimple.setLocalMatrix(matrix);
 	canvas.drawCircle(x, y, MARGIN, paint);
 
 	paint.setShader(null);
 	canvas.drawBitmap(previous, -width / 2 + MARGIN, - height / 2 + MARGIN, paint);
 	canvas.drawBitmap(next, width / 2 - next.getHeight() - MARGIN,
-			-height / 2 + MARGIN, paint);	
+			  -height / 2 + MARGIN, paint);	
     }
 
     // On touch event
@@ -150,6 +158,8 @@ public class Knob extends View
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
+	detector.onTouchEvent(event);
+
 	float x = event.getX() - width / 2;
 	float y = event.getY() - height / 2;
 
@@ -159,20 +169,22 @@ public class Knob extends View
 	{
 	case MotionEvent.ACTION_DOWN:
 
-	     float radius = (float) Math.hypot(x, y);
+	    float radius = (float) Math.hypot(x, y);
 
-	     if (radius > Math.min(width, height) / 2.0 && y < 0.0)
-	     {
-	    	 if (x < 0.0)
-	    		 value -= 1.0;
-	    	 else
-	    		 value += 1.0;
+	    if (radius > Math.min(width, height) / 2.0 && y < 0.0)
+	    {
+		if (x < 0.0)
+		    value -= 1.0;
+		else
+		    value += 1.0;
 
-	 		if (listener != null)
-	 			listener.onKnobChange(this, value);
+		value = Math.round(value);
 
-	    	  invalidate();
-	     }
+		if (listener != null)
+		    listener.onKnobChange(this, value);
+
+		invalidate();
+	    }
 	    break;
 
 	case MotionEvent.ACTION_MOVE:
@@ -196,7 +208,7 @@ public class Knob extends View
 
 		// Update value
 
-		value += Math.round(delta * 100 / Math.PI);
+		value += delta * 100 / Math.PI;
 
 		if (value < MIN)
 		    value = MIN;
@@ -205,7 +217,7 @@ public class Knob extends View
 		    value = MAX;
 
 		if (listener != null)
-			listener.onKnobChange(this, value);
+		    listener.onKnobChange(this, value);
 
 		invalidate();
 	    }
@@ -219,6 +231,71 @@ public class Knob extends View
 	return true;
     }
 
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			   float velocityY)
+    {
+	// Get event coordinates
+
+	float x1 = e1.getX() - width / 2;
+	float y1 = e1.getY() - height / 2;
+
+	float x2 = e2.getX() - width / 2;
+	float y2 = e2.getY() - height / 2;
+
+	// Calculate angles
+
+	float theta1 = (float)Math.atan2(x1, -y1);
+	float theta2 = (float)Math.atan2(x2, -y2);
+
+	// Calculate difference and absolute velocity
+
+	float delta = theta2 - theta1;
+	float velocity = (float) Math.abs(Math.hypot(velocityX, velocityY));
+
+	// Allow for crossing origin
+
+	if (delta > Math.PI)
+	    delta -= 2.0 * Math.PI;
+
+	if (delta < -Math.PI)
+	    delta += 2.0 * Math.PI;
+
+	// Calculate target value for animator
+
+	float target = value + (float)(delta * velocity / 250 * Math.PI);
+
+	animator = ValueAnimator.ofFloat(value, target);
+	animator.setInterpolator(new DecelerateInterpolator());
+	animator.addUpdateListener(this);
+	animator.start();
+
+	return true;
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation)
+    {
+	value = (Float) animation.getAnimatedValue();		
+
+	if (value < MIN)
+	{
+	    animation.cancel();
+	    value = MIN;
+	}
+
+	if (value > MAX)
+	{
+	    animation.cancel();
+	    value = MAX;
+	}
+
+	if (listener != null)
+	    listener.onKnobChange(this, value);
+
+	invalidate();
+    }
+
     protected void setOnKnobChangeListener(OnKnobChangeListener l)
     {
 	listener = l;
@@ -227,5 +304,30 @@ public class Knob extends View
     public interface OnKnobChangeListener
     {
 	public abstract void onKnobChange(Knob knob, float value);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e)
+    {
+	return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {}
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			    float distanceY)
+    {
+	return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {}
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e)
+    {
+	return false;
     }
 }
